597814651fcea4075d4dff5d4b52ed7133c66344
cesarsiguencia <cesar.siguencia@gmail.com>
2022-01-23 17:52:38 -0500

    deleting unused variables

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,6 @@
 var textArea = document.getElementById("password");
 var generateBtn = document.querySelector("#generate");
 
-
-var result = '';
 var uppercaseArray = [ 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',  'S', 'T', 'U', 'V', 'W', 'X','Y', 'Z' ];
 var lowercaseArray = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 var numbersArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
@@ -125,7 +123,6 @@ var clearPassword = function(){
   //if user wants a new password, the following fields are cleared out
   textArea.innerHTML = "";
   confirmedCharacters = [];
-  return confirmedCharacters;
 }
 
 
